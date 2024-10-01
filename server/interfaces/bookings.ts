f98ff597b0af9681f79759704b2d8c40a9a3a5f0
JulianKings <julli123@hotmail.es>
2024-10-01 16:19:13 +0200

    feat: Finish seed-sql and start working on the app

--- server/interfaces/bookings.ts
+++ server/interfaces/bookings.ts
@@ -1,11 +1,10 @@
-import { Types } from "mongoose";
 import { ApiAbstractInterface } from "./apiManagement";
 
 export interface ApiBookingInterface extends ApiAbstractInterface {
     customer_name: string,
     date: Date,
     status: 'checking_out' | 'checking_in' | 'in_progress',
-    room: Types.ObjectId | string,
+    room: number,
     check_in: Date,
     check_out: Date,
     notes: string    
